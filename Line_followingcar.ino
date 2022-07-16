b55bab1bc70905d7428ef6372c91e1472ecd77f4
Fahad <88250498+MDfahad2001@users.noreply.github.com>
2022-07-16 23:08:14 +0530

    code

---
+++ Line_followingcar.ino
@@ -0,0 +1,54 @@
+#include <AFMotor.h>
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+void setup() {
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  Serial.begin(9600);  
+}
+void loop(){
+  Serial.println(digitalRead(A0)); 
+  Serial.println(digitalRead(right));
+  if(digitalRead(A0)==0 && digitalRead(A1)==0){
+    motor1.run(FORWARD);
+    motor1.setSpeed(150);
+    motor2.run(FORWARD);
+    motor2.setSpeed(150);
+    motor3.run(FORWARD);
+    motor3.setSpeed(150);
+    motor4.run(FORWARD);
+    motor4.setSpeed(150);
+  }
+  else if(digitalRead(A0)==0 && !digitalRead(A1)==0){
+    motor1.run(FORWARD);
+    motor1.setSpeed(200);
+    motor2.run(FORWARD);
+    motor2.setSpeed(200);
+    motor3.run(BACKWARD);
+    motor3.setSpeed(200);
+    motor4.run(BACKWARD);
+    motor4.setSpeed(200);   
+  }
+  else if(!digitalRead(A0)==0 && digitalRead(A1)==0){
+    motor1.run(BACKWARD);
+    motor1.setSpeed(200);
+    motor2.run(BACKWARD);
+    motor2.setSpeed(200);
+    motor3.run(FORWARD);
+    motor3.setSpeed(200);
+    motor4.run(FORWARD);
+    motor4.setSpeed(200);
+  }
+  else if(!digitalRead(A0)==0 && !digitalRead(A1)==0){
+    motor1.run(RELEASE);
+    motor1.setSpeed(0);
+    motor2.run(RELEASE);
+    motor2.setSpeed(0);
+    motor3.run(RELEASE);
+    motor3.setSpeed(0);
+    motor4.run(RELEASE);
+    motor4.setSpeed(0);
+  }
+}
